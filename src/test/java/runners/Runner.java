455771d2be849a29373f29c68be6ce815a8b22e0
muzafferugur <av.muzafferugur@gmail.com>
2023-05-23 01:20:02 +0300

    23.05.2023

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags =  "@java",//ikisine aynı ismi verip iki scenarioyu da çalıştırabiliriz
+        tags =  "@config",//ikisine aynı ismi verip iki scenarioyu da çalıştırabiliriz
         dryRun = false
 
 )
@@ -22,6 +22,14 @@ public class Runner {
 
     @RunWith(Cucumber.class) notasyonu Runner class'ina calisma özelliği katar.
     Bu notasyon olduğu için Cucumber framework'ümüzde JUnit kullanmayı tercih ediyoruz.
+
+    features: Runner dosyasının feature dosyalarını nereden bulacağını tarif eder.
+    glue: step definitions dosyalarını nereden bulacağını tarif eder.
+    tags: o an hangi tag'ı çalıştırmak istiyorsak onu belli eder.
+    dryRun: iki seçenek var
+    ı.dryRun=true => yazdığımızda Testimizi çalıştırmadan sadece eksik adımları bize verir./sadeece kontrol eder eksik adım var mı yok mu, hiç bir şeyi çalıştırmaz
+    ıı.dryRun=false => yazdığımızda Testlerimizi çalıştırır.
+
 
      */
 }
