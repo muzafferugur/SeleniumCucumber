22b9d3609015fd56ffd5ec4380300227ce5d2f94
muzafferugur <av.muzafferugur@gmail.com>
2023-05-23 21:05:36 +0300

    23.05.2023

--- src/test/java/stepDefinitions/hmcStepDefinitions.java
+++ src/test/java/stepDefinitions/hmcStepDefinitions.java
@@ -1,31 +1,51 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import pages.hmcPage;
 import utilities.ConfigReader;
 
 public class hmcStepDefinitions {
-    hmcPage hmcPage=new hmcPage();
+    hmcPage hmcPage = new hmcPage();
+
     @Then("Log in  yazisina tiklar")
     public void log_in_yazisina_tiklar() {
         hmcPage.loginButonu.click();
     }
+
     @Then("gecerli username girer")
     public void gecerli_username_girer() {
         hmcPage.usernameKutusu.sendKeys(ConfigReader.getProperties("hmcValidUsername"));
     }
+
     @Then("gecerli password girer")
     public void gecerli_password_girer() {
         hmcPage.passwordKutusu.sendKeys(ConfigReader.getProperties("hmcValidPassword"));
     }
+
     @Then("Login butonuna basar")
     public void login_butonuna_basar() {
         hmcPage.loginButonu2.click();
     }
+
     @Then("sayfaya giris yaptigini kontrol eder")
     public void sayfaya_giris_yaptigini_kontrol_eder() {
         Assert.assertTrue(hmcPage.hotelManagement.isDisplayed());
     }
 
+    @And("gecersiz password girer")
+    public void gecersizPasswordGirer() {
+        hmcPage.passwordKutusu.sendKeys(ConfigReader.getProperties("hmcWrongPassword"));
+    }
+
+    @Then("sayfaya giris yapilamadigini test eder")
+    public void sayfayaGirisYapilamadiginiTestEder() {
+        Assert.assertTrue(hmcPage.loginButonu2.isDisplayed());
+    }
+
+    @And("gecersiz username girer")
+    public void gecersizUsernameGirer() {
+        hmcPage.usernameKutusu.sendKeys(ConfigReader.getProperties("hmcWrongUsername"));
+    }
 }
---
+++ target/test-classes/features/US1006_hotelMyCampNegativeLogin.feature
@@ -0,0 +1,15 @@
+# hotel my camp sitesinde 3 adet negative test senaryosu oluşturalım
+  #1- doğru username-yanlış password
+  #2- yanlış username-doğru password
+  #3- yanlış username-yanlış password
+
+Feature: US1006 yanlis bilgilerle siteye giris yapilamaz
+
+  Scenario: TC09 yanlis password ile giris yapilamaz
+    Given kullanici "hmcUrl" anasayfasinda
+    Then Log in  yazisina tiklar
+    And gecerli username girer
+    And gecersiz password girer
+    And Login butonuna basar
+    Then sayfaya giris yapilamadigini test eder
+    And sayfayi kapatir
