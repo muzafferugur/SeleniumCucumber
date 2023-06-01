57be5d33b0f275ce2874245ea09e8b94febc7377
muzafferugur <av.muzafferugur@gmail.com>
2023-06-01 11:46:11 +0300

    01.06.2023

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -6,71 +6,64 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
+
 
 import java.time.Duration;
 
 public class Driver {
-    /*
-    POM'de Driver için TestBase Classına extend etmek yerine  Driver classından
-    static methodlarkullanarak driver oluşturulup,ilgili ayarların yapılması ve en sonda
-    driverın kapatılması tercih edilmiştir.
-
-    POM'de Driver classındaki getDriver methodunun obje oluşturularak kullanılmasını
-    engellemek için  Singleton pattern kullanımı benimsenmiştir.
-
-    Singleton pattern : Tekli kullanım, bir classın farklı classlardan obje oluşturularak kullanımını
-    engellemek için kullanılır.
-
-    Bunu sağlamak için yapmamız gereken şey oldukça basit obje oluşturmak için kullanılan
-    constructoru private yaptığınızda baska classlarda Driver classından obje oluşturulması
-    mümkün OLAMAZ.
-     */
-
-    private Driver() {
-
-    }
-
     static WebDriver driver;
 
     public static WebDriver getDriver() {
-
         if (driver == null) {
             switch (ConfigReader.getProperty("browser")) {
-
+                case "chrome":
+                    ChromeOptions options=new ChromeOptions();
+                    options.addArguments("--incognito");
+                    options.addArguments("--ignore-certificate-errors");
+                    options.addArguments("--allow-insecure-localhost");
+                    options.addArguments("--acceptInsecureCerts");
+                    options.addArguments("--disable-blink-features=AutomationControlled");
+                    options.addArguments("--disable-blink-features");
+                    options.addArguments("--start-maximized");
+                    options.addArguments("--disable-extensions");
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver(options);
+                    break;
                 case "edge":
                     WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
+                    driver=new EdgeDriver();
                     break;
-
-                case "safari":
-                    WebDriverManager.safaridriver().setup();
-                    driver = new SafariDriver();
-                    break;
-
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                    driver=new FirefoxDriver();
                     break;
 
+                case "headless-chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
                 default:
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-
+                    driver=new ChromeDriver();
 
             }
 
-
-            //driver.manage().window().maximize();
+            driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
         return driver;
     }
 
     public static void closeDriver() {
-
-        if (driver != null) {//drivera değer atanmışsa demek
+        if (driver != null) {
             driver.close();
+            driver = null;
+        }
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
             driver = null;
         }
     }
