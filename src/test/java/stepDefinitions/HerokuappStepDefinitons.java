7b9e40110d8968e46a8c9d832b011eccd75be488
muzafferugur <av.muzafferugur@gmail.com>
2023-06-02 19:58:20 +0300

    02.06.2023

--- src/test/java/stepDefinitions/HerokuappStepDefinitons.java
+++ src/test/java/stepDefinitions/HerokuappStepDefinitons.java
@@ -4,10 +4,12 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.HerokuappPage;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 import java.time.Duration;
@@ -21,6 +23,17 @@ public abstract class HerokuappStepDefinitons {
         herokuappPage.addElementButton.click();
     }
 
+    @Then("Delete butonu gorunur oluncaya kadar bekler")
+    public void deleteButonuGorunurOluncayaKadarBekler() {
+/**
+ * Delete butonu gorunur oluncaya kadar bekleme sürecini biz selenium-javada yer alan
+ * Expilicitly Wait:=>Çoğunlukla belirli öğeler için belirli koşul(expected condition) için kullanılır.(şartlı bekleme) kullanıyoruz.
+ * Bu durumu gerçekleştirmek açısından yeni bir wait tanımlanarak ExpectedConditions.visibilityOf() methodu ile şartlı beklemeyi sağlamış bulunmaktayız.
+ */
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(herokuappPage.deleteButton));
+    }
+
     @Then("Delete butonunun gorunur oldugunu test eder")
     public void delete_butonunun_gorunur_oldugunu_test_eder() {
         Assert.assertTrue(herokuappPage.deleteButton.isDisplayed());
@@ -30,5 +43,6 @@ public abstract class HerokuappStepDefinitons {
     public void delete_butonuna_basar() {
         herokuappPage.deleteButton.click();
     }
+
 
 }
--- src/test/resources/features/US1010_Herokuapp.feature
+++ src/test/resources/features/US1010_Herokuapp.feature
@@ -5,6 +5,7 @@ Feature: US1010 herokuapp Delete testi
 
     Given kullanici "herokuappUrl" anasayfasinda
     And add element butonuna basar
+    Then Delete butonu gorunur oluncaya kadar bekler
     And Delete butonunun gorunur oldugunu test eder
     Then Delete butonuna basar
     And sayfayi kapatir
